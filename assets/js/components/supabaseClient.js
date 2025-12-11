8620fd3a90da8856be3620d5e93fc7bb05d141af
Daniel Wolfe <daniel.scheerhorn.wolfe@gmail.com>
2025-12-11 10:03:51 -0600

    fixing null auth stuff

--- assets/js/components/supabaseClient.js
+++ assets/js/components/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 export const supabase = createClient(
     'https://fxpaqqpddrcunxcwnjgk.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ4cGFxcXBkZHJjdW54Y3duamdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3MDAwNzcsImV4cCI6MjA3NTI3NjA3N30.BlCa7aLn9XneDhElnCtwR8YuuxGvTR_8bc2aAkivmcQ'
+    'sb_publishable_AmyQ5z5fnhOX8KL5pySplQ_Td16NI02'
 );
